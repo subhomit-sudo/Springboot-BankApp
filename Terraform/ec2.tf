9f965dfdbd76262201450796d0d34724e5f02d8c
Subhojit Mitra <subhojitmitra464@gmail.com>
2025-05-05 12:33:16 +0530

    added terraform and dockerfile

---
+++ Terraform/ec2.tf
@@ -0,0 +1,77 @@
+data "aws_ami" "os_image" {
+  owners = ["099720109477"]
+  most_recent = true
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/*amd64*"]
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "bankapp-automate-key"
+  public_key = file("bankapp-automate-key.pub")
+}
+
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "allow_user_to_connect" {
+  name        = "allow TLS"
+  description = "Allow user to connect"
+  vpc_id      = aws_default_vpc.default.id
+  ingress {
+    description = "port 22 allow"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = " allow all outgoing traffic "
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "port 80 allow"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "port 443 allow"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "bankapp-security"
+  }
+}
+
+resource "aws_instance" "testinstance" {
+  ami             = data.aws_ami.os_image.id
+  instance_type   = var.instance_type
+  key_name        = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.allow_user_to_connect.name]
+  tags = {
+    Name = "Bankapp-Automation-Server"
+  }
+  root_block_device {
+    volume_size = 30 
+    volume_type = "gp3"
+  }
+
+}
